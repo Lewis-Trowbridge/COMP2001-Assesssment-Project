0508bdd317bf8b438fcd69e054f570dae2fa8d04
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-21 14:46:42 +0000

    Added function to get 2D array of hospital data, and hospital data CSV (obtained from https://plymouth.thedata.place/dataset/air-quality-data)

--- Hospitals/public/index.php
+++ Hospitals/public/index.php
@@ -1,7 +1,10 @@
+<?php
+include_once "../src/get_hospital_array.php";
+?>
 <head>
 
     <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
 </head>
 <body>
 
---
+++ Hospitals/src/get_hospital_array.php
@@ -0,0 +1,12 @@
+<?php
+function get_hospital_array(){
+    $hospital_array = array();
+    $filename = "../assets/csv/resources_cd162ad1-d7d5-42a9-b1ab-0edbcd697f1e_air-quality-by-pm2.5-score-blf.org.uk.csv";
+    $csv_file = fopen($filename, "r");
+    while (feof($csv_file) == false){
+        $csv_line_string = fgets($csv_file);
+        $hospital_array[] = str_getcsv($csv_line_string);
+    }
+    fclose($csv_file);
+    return $hospital_array;
+}
