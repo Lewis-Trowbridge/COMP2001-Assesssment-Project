e677c777cf4366a624e09cbf9ff4107d23021bfa
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-24 17:14:50 +0000

    LDA: Edited practices endpoint to display JSON-LD of CSV dataset

--- Hospitals/public/practices.php
+++ Hospitals/public/practices.php
@@ -1,7 +1,12 @@
 <?php
 include_once "../src/get_json_context.php";
+include_once "../src/get_practice_object_array.php";
 
 $context_string = get_json_context();
+$practice_objects = get_practice_object_array();
 header("Content-Type: application/json");
-echo $context_string;
-
+$context_array = json_decode($context_string);
+foreach ($practice_objects as $practice_object){
+    $context_array->places[] = $practice_object;
+}
+echo json_encode($context_array);
