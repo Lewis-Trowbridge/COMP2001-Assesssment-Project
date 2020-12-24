cf0e00d4f08ca7e3d4c150d7463503b4dfabf0ab
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-24 17:13:38 +0000

    LDA: Added method to easily obtain array of practice objects

---
+++ Hospitals/src/get_practice_object_array.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once "get_hospital_array.php";
+include_once "Practice.php";
+
+function get_practice_object_array(){
+    $practice_array = get_hospital_array();
+    $practice_objects = array();
+    foreach ($practice_array as $single_practice_array){
+        $practice_objects[] = new Practice($single_practice_array);
+    }
+    return $practice_objects;
+}
