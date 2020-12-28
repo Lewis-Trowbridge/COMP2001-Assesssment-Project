28ab4513b0bf2978b1084c0412340aab61433f49
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-28 17:23:26 +0000

    LDA: Added ability to display data as table in HTML

--- Hospitals/public/data.php
+++ Hospitals/public/data.php
@@ -1,3 +1,6 @@
+<?php
+include_once "../src/get_hospital_array.php";
+?>
 <head>
     <title>Data</title>
     <link href="../assets/css/data.css"  rel="stylesheet"/>
@@ -14,6 +17,34 @@
         <div class="row">
             <div id="map" class="vh-100"></div>
         </div>
+
+        <div class="row">
+            <table class="table">
+                <?php
+                $practice_array = get_hospital_array();
+                $header_array = $practice_array[0];
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th scope='col'>Number</th>";
+                foreach ($header_array as $header_name){
+                    echo "<th scope='col'>$header_name</th>";
+                }
+                echo "</tr>";
+                echo "</thead>";
+
+                echo "<tbody>";
+                for ($offset = 1; $offset < count($practice_array); $offset++ ){
+                    $practice = $practice_array[$offset];
+                    echo "<tr>";
+                    echo "<th scope='row'>$offset</th>";
+                    foreach ($practice as $practice_item) {
+                        echo "<td>$practice_item</td>";
+                    }
+                    echo "</tr>";
+                }
+                ?>
+            </table>
+        </div>
     </div>
 
     <?php
@@ -22,6 +53,7 @@
 
     <script src="../assets/js/data.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD5O75uQCZ68icAZQGbn5tKoCbJ4MEVnRg&callback=initMap" async defer></script>
 
 </body>
