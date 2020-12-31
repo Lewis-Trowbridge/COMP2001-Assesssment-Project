6be400781da7938cff434f83ee0d6140532cdbae
Lewis Trowbridge <lewbob9927@gmail.com>
2020-12-31 21:50:32 +0000

    LDA: Postcode page makes call to API and echoes back necessary data obtained

--- Hospitals/public/postcode.php
+++ Hospitals/public/postcode.php
@@ -10,7 +10,29 @@ include_once "header.php";
 ?>
 
 <div class="container">
-
+    <?php
+    $postcode = $_GET["postcode"];
+    if ($postcode == null){
+        echo "<p>No postcode received - please try again</p>";
+    }
+    else {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "http://api.postcodes.io/postcodes/" . $postcode);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        $json_output = json_decode($output);
+        if ($json_output->status == 200){
+            $postcode_results = $json_output->result;
+            echo "<p>$postcode_results->postcode</p>";
+            echo "<p>$postcode_results->latitude</p>";
+            echo "<p>$postcode_results->longitude</p>";
+        }
+        else {
+            echo "<p>Sorry, but we cannot find your postcode.</p>";
+        }
+    }
+    ?>
 </div>
 
 <?php
