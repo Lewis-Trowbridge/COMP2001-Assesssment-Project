59f3246099fb2640bc55a3af56f42132afde01e5
Lewis Trowbridge <lewbob9927@gmail.com>
2021-01-03 14:12:38 +0000

    LDA: Postcode search page displays relevant information about closest practice as shown in wireframe

--- Hospitals/public/postcode.php
+++ Hospitals/public/postcode.php
@@ -1,3 +1,7 @@
+<?php
+include_once "../src/get_closest_practice.php";
+?>
+
 <head>
     <title>Postcode</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
@@ -11,6 +15,14 @@ include_once "header.php";
 
 <div class="container">
     <?php
+
+    function echo_in_row_div($content)
+    {
+        echo "<div class='row'>";
+        echo $content;
+        echo "</div>";
+    }
+
     $postcode = $_GET["postcode"];
     if ($postcode == null){
         echo "<p>No postcode received - please try again</p>";
@@ -24,9 +36,14 @@ include_once "header.php";
         $json_output = json_decode($output);
         if ($json_output->status == 200){
             $postcode_results = $json_output->result;
-            echo "<p>$postcode_results->postcode</p>";
-            echo "<p>$postcode_results->latitude</p>";
-            echo "<p>$postcode_results->longitude</p>";
+            echo "<div class='text-center'>";
+            $closest_practice = get_closest_practice($postcode_results->latitude, $postcode_results->longitude);
+            echo_in_row_div("<p>Your nearest practice:</p>");
+            echo_in_row_div("<h2>$closest_practice->name</h2>");
+            $practice_address = $closest_practice->address;
+            echo_in_row_div("<p>$practice_address->postcode</p>");
+            echo_in_row_div("<h3>$closest_practice->pm2_5 PM<sub>2.5</sub> μg/m<sup>3</sup></h3>");
+            echo "</div>";
         }
         else {
             echo "<p>Sorry, but we cannot find your postcode.</p>";
--- Hospitals/src/get_closest_practice.php
+++ Hospitals/src/get_closest_practice.php
@@ -7,8 +7,6 @@ function get_closest_practice($postcode_lat, $postcode_lng){
     $closest_distance = 0;
     foreach ($practice_objects as $practice){
         $distance = sqrt((($postcode_lat - $practice->geo->latitude) ** 2) + (($postcode_lng - $practice->geo->longitude) ** 2));
-        echo $distance;
-        echo "<br>";
         if ($distance < $closest_distance or $closest_distance == 0){
             $closest_distance = $distance;
             $closest = $practice;
