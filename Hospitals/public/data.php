785b611f988202895882ec7e76b74f980659d6e2
Lewis Trowbridge <lewbob9927@gmail.com>
2021-01-20 10:42:49 +0000

    LDA: Added language statement to pages

--- Hospitals/public/data.php
+++ Hospitals/public/data.php
@@ -1,6 +1,7 @@
 <?php
 include_once "../src/get_hospital_array.php";
 ?>
+<html lang="en">
 <head>
 
     <meta charset="UTF-8">
@@ -10,51 +11,52 @@ include_once "../src/get_hospital_array.php";
 </head>
 
 <body>
-    <?php
-    include_once "header.php";
-    ?>
+<?php
+include_once "header.php";
+?>
 
-    <div class="container">
-        <div class="row h-100">
-            <div id="map"></div>
-        </div>
-
-        <div class="row table-responsive">
-            <table id="data-table" class="table">
-                <?php
-                $practice_array = get_hospital_array(true);
-                $header_array = $practice_array[0];
-                echo "<thead>";
-                echo "<tr>";
-                echo "<th scope='col'>Number</th>";
-                foreach ($header_array as $header_name){
-                    echo "<th scope='col'>$header_name</th>";
-                }
-                echo "</tr>";
-                echo "</thead>";
-
-                echo "<tbody>";
-                for ($offset = 1; $offset < count($practice_array); $offset++ ){
-                    $practice = $practice_array[$offset];
-                    echo "<tr>";
-                    echo "<th scope='row'>$offset</th>";
-                    foreach ($practice as $practice_item) {
-                        echo "<td>$practice_item</td>";
-                    }
-                    echo "</tr>";
-                }
-                ?>
-            </table>
-        </div>
+<div class="container">
+    <div class="row h-100">
+        <div id="map"></div>
     </div>
 
-    <?php
-    include_once "footer.php";
-    ?>
+    <div class="row table-responsive">
+        <table id="data-table" class="table">
+            <?php
+            $practice_array = get_hospital_array(true);
+            $header_array = $practice_array[0];
+            echo "<thead>";
+            echo "<tr>";
+            echo "<th scope='col'>Number</th>";
+            foreach ($header_array as $header_name){
+                echo "<th scope='col'>$header_name</th>";
+            }
+            echo "</tr>";
+            echo "</thead>";
 
-    <script src="../assets/js/data.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD5O75uQCZ68icAZQGbn5tKoCbJ4MEVnRg&callback=initMap" async defer></script>
+            echo "<tbody>";
+            for ($offset = 1; $offset < count($practice_array); $offset++ ){
+                $practice = $practice_array[$offset];
+                echo "<tr>";
+                echo "<th scope='row'>$offset</th>";
+                foreach ($practice as $practice_item) {
+                    echo "<td>$practice_item</td>";
+                }
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    </div>
+</div>
+
+<?php
+include_once "footer.php";
+?>
+
+<script src="../assets/js/data.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD5O75uQCZ68icAZQGbn5tKoCbJ4MEVnRg&callback=initMap" async defer></script>
 
 </body>
+</html>
--- Hospitals/public/index.php
+++ Hospitals/public/index.php
@@ -1,3 +1,4 @@
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Home</title>
@@ -12,9 +13,9 @@ include_once "header.php";
 <section class="container">
     <h2>Welcome</h2>
     <p>In this web application, data is presented about the air quality surrounding hospitals and GP practices in
-    Plymouth based on the density of PM<sub>2.5</sub> in the air - you can inspect the overall picture on the map
-    or check the average for each town in the wider Plymouth area <a href="data.php">here.</a> A postcode lookup is
-    also available if you want to check the quality of your local practice.</p>
+        Plymouth based on the density of PM<sub>2.5</sub> in the air - you can inspect the overall picture on the map
+        or check the average for each town in the wider Plymouth area <a href="data.php">here.</a> A postcode lookup is
+        also available if you want to check the quality of your local practice.</p>
 
     <p>The original dataset used can be found <a href="https://plymouth.thedata.place/dataset/air-quality-data">here</a>, available under the <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence</a> .</p>
 
@@ -28,3 +29,4 @@ include_once "footer.php";
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
 </body>
+</html>
