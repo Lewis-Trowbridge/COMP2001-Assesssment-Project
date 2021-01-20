f769755a53ed8f73351a98aab5e89961edc97fcd
Lewis Trowbridge <lewbob9927@gmail.com>
2021-01-20 10:36:22 +0000

    LDA: Added charset clarification to pages

--- Hospitals/public/data.php
+++ Hospitals/public/data.php
@@ -2,6 +2,8 @@
 include_once "../src/get_hospital_array.php";
 ?>
 <head>
+
+    <meta charset="UTF-8">
     <title>Data</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
 
--- Hospitals/public/towns.php
+++ Hospitals/public/towns.php
@@ -3,6 +3,7 @@ include_once "../src/get_towns_average_array.php";
 ?>
 
 <head>
+    <meta charset="UTF-8">
     <title>Towns</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous" />
 </head>
